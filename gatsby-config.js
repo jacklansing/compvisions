eff6848d048416ca954d520995b89b4fc1261ee4
Jack Lansing <jack.lansing@live.com>
2020-06-28 11:22:13 -0400

    feat: added MDX lib for pulling in markdown as components

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,14 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/data/pages`,
+      },
+    },
+    `gatsby-plugin-mdx`,
     `gatsby-transformer-csv`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Test from '../data/pages/index/index2.mdx';
+import Paper from '@material-ui/core/Paper';
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
       <h1>Home</h1>
+      <Paper elevation={3} style={{ padding: '1rem' }}>
+        <Test />
+      </Paper>
     </Layout>
   );
 };
