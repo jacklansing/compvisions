d59cc710ac146932b9726a14315efddf329e8d68
Jack Lansing <jack.lansing@live.com>
2020-06-28 11:34:19 -0400

    feat: added basic page layout

---
+++ src/pages/consulting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Consulting = () => {
+  return (
+    <Layout>
+      <SEO title="Consulting" />
+    </Layout>
+  );
+};
+
+export default Consulting;
---
+++ src/pages/remote-learning.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const RemoteLearning = () => {
+  return (
+    <Layout>
+      <SEO title="RemoteLearning" />
+    </Layout>
+  );
+};
+
+export default RemoteLearning;
